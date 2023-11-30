c725c35ddd88d830ce842e219263c84be2835eec
MoonStar <moonsung0331@gmail.com>
2023-11-30 22:30:40 +0900

    Kafka 구축 - Simple Access Logging Pipeline 구축

---
+++ banking-service/src/main/java/org/moonpay/banking/BankingConfig.java
@@ -0,0 +1,9 @@
+package org.moonpay.banking;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.moonpay.common")
+public class BankingConfig {
+}
---
+++ common/src/main/java/org/moonpay/common/LoggingProducer.java
@@ -0,0 +1,45 @@
+package org.moonpay.common;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Properties;
+
+@Component
+public class LoggingProducer {
+
+    private final KafkaProducer<String, String> producer;
+
+    private final String topic;
+
+    public LoggingProducer(@Value("${kafka.clusters.bootstrapservers}") String bootstrapServers,
+                           @Value("${logging.topic}")  String topic) {
+
+        // Producer Initialization
+        Properties props = new Properties();
+
+        // kafka:20902
+        props.put("bootstrap.servers", bootstrapServers);
+
+        // "key:value"
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        this.producer = new KafkaProducer<>(props);
+        this.topic = topic;
+    }
+
+    // Kafka Cluster [key, value] Produce
+    public void sendMessage(String key, String value) {
+        ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, value);
+        producer.send(record, ((metadata, exception) -> {
+            if (exception == null) {
+
+            } else {
+                exception.printStackTrace();
+            }
+        }));
+    }
+}
